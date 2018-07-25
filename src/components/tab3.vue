bd6e9af106c33d5dbbe757317624296cc10e63c7
yangjin <826288352@qq.com>
2018-07-25 12:21:54 +0800

    add

---
+++ src/components/tab3.vue
@@ -0,0 +1,13 @@
+<template>
+<div>3333</div>
+</template>
+
+<script>
+export default {
+  name: 'tab3'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/page/content.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>content page</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
